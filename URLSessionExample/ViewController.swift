99da9fb4d0ca8123f550c41874042581b33c4f9c
Rus <de.mr44@gmail.com>
2021-12-23 23:08:35 +0500

    Added getting data from an API with URLSession

--- URLSessionExample/ViewController.swift
+++ URLSessionExample/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let session = URLSession.shared
+    let sessionConfiguration = URLSessionConfiguration.default
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        obtainPosts()
     }
-
-
+    
+    func obtainPosts() {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        let dataTask = session.dataTask(with: url) { data, response, error in
+            if error == nil {
+                print("Success!")
+            }
+        }
+        
+        dataTask.resume()
+    }
 }
 
