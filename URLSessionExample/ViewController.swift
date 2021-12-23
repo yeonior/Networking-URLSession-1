cf51663218d04432133ba82b07b108e5a5b51243
Rus <de.mr44@gmail.com>
2021-12-24 00:21:46 +0500

    Finalized the parsing data func

--- URLSessionExample/ViewController.swift
+++ URLSessionExample/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
     
     let session = URLSession.shared
     let sessionConfiguration = URLSessionConfiguration.default
+    let decoder = JSONDecoder()
+    var posts = [Post]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,9 +24,21 @@ class ViewController: UIViewController {
         
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
         
-        let dataTask = session.dataTask(with: url) { data, response, error in
-            if error == nil {
-                print("Success!")
+        let dataTask = session.dataTask(with: url) { [weak self] data, response, error in
+            
+            guard let self = self else { return }
+            
+            if error == nil, let parseData = data {
+                
+                do {
+                    self.posts = try self.decoder.decode([Post].self, from: parseData)
+                    print(self.posts)
+                } catch {
+                    print("Parsing went wrong!")
+                }
+                
+            } else if let error = error {
+                print("Error: \(error.localizedDescription)")
             }
         }
         
